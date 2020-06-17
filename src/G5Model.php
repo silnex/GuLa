304ef4dfeb94dccbc19c658ae9782ee22694e77c
silnex <silnex@silnex.kr>
2020-06-18 01:41:44 +0900

    Add config

--- src/G5Model.php
+++ src/G5Model.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class G5Model extends Model
 {
-    protected $connection = 'gnuboard';
+    protected $connection = 'gula.connection';
 }
---
+++ src/Models/config/gula.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'connection' => [
+        'driver'    => 'mysql',
+        'host'      => 'localhost',
+        'database'  => '',
+        'username'  => '',
+        'password'  => '',
+        'charset'   => 'utf8',
+        'collation' => 'utf8_unicode_ci',
+        'prefix'    => '',
+    ]
+];
