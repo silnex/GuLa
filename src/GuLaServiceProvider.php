a048ef8349e418c1fd809dc705fcaa1270b2c92c
silnex <silnex@silnex.kr>
2020-06-18 01:37:51 +0900

    Add service provider

---
+++ src/GuLaServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace SilNex\GuLa;
+
+use Illuminate\Support\ServiceProvider;
+
+class GuLaServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        // 
+    }
+
+    public function boot()
+    {
+        // 
+    }
+}
