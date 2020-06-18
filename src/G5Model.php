c7f4b2070ba7dcef75a1d809d590b57da7fd879b
silnex <silnex@silnex.kr>
2020-06-18 18:29:33 +0900

    change timestamp control location

--- src/G5Model.php
+++ src/G5Model.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class G5Model extends Model
 {
+    public $timestamps = false;
+
     protected $connection = 'gula';
 }
--- src/Models/Young/G5ShopInicisLog.php
+++ src/Models/Young/G5ShopInicisLog.php
@@ -7,13 +7,6 @@ use SilNex\GuLa\G5Model;
 class G5ShopInicisLog extends G5Model
 {
     /**
-     * 디폴트 타임 스템프 사용 안함
-     *
-     * @var string
-     */
-    public $timestamps = false;
-
-    /**
      * 그누보드 테이블
      *
      * @var string
